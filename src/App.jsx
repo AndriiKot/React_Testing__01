89119f7c2f4d24730a47866c6c767f00575d6fb9
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 14:57:21 +0200

    React 'React_Testing__01' ! testing 'npm run dev' 2 !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  console.log("Hi!")
 
   return (
     <>
       <h1>React Testing 01</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
