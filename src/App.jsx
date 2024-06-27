36b62b92f97c4cfb9f02d0f25d803f2aac2fc234
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 19:04:04 +0200

    React 'React_Testing__01' ! RESTART PROJECT !

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import "./App.css";
 import fetchCoursid from "./FetchCoursid/FetchCoursid";
-import { useQuery } from "react-query"
 
 function App() {
-  fetchCoursid();
-  // const { data, isLoading, error } = useQuery('coursid', fetchCoursid)
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>
-  // }
-
-  // if (error) {
-  //   return <div>Error: {error.message}</div>
-  // }
-
   return (
     <>
       <h1>React Testing 01</h1>
--- src/FetchCoursid/FetchCoursid.jsx
+++ src/FetchCoursid/FetchCoursid.jsx
@@ -1,9 +1,1 @@
-﻿const fetchCoursid = async () => {
-  const data = await fetch("https://api.coingate.com/v2/rates/merchant").then(
-    (response) => response.json()
-  );
-  console.log("hi!")
-  return data;
-};
-
-export default fetchCoursid;
+﻿
