3d55156d2e5a14caa8b898b2c390411fd9eab1e2
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 15:24:10 +0200

    React 'React_Testing__01' !  testing  fetchCoursid testing 1 !

--- src/FetchCoursid/FetchCoursid.jsx
+++ src/FetchCoursid/FetchCoursid.jsx
@@ -2,7 +2,8 @@
   const data = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11").then(
     (response) => response.json()
   );
-  console.log(data);
+  console.log("fetchCoursid", data);
+  // console.log(data);
   return data;
 };
 
