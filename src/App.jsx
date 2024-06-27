8b4579ac4cacb872c601b8c40a0067344d0c61e2
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 15:55:01 +0200

    React 'React_Testing__01' !  testing <loading .. > !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import fetchCoursid from "./FetchCoursid/FetchCoursid";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query"
 
 function App() {
-  const { isLoading, isError, data } = useQuery({
-    queryKey: "coursid",
-    queryFn: fetchCoursid
-  });
+
+  const { data, isLoading, error } = useQuery('coursid', fetchCoursid)
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   return (
     <>
       <h1>React Testing 01</h1>
