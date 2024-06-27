7440cd48749d9cd281551105b9ed1be79fae5a7b
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 15:33:01 +0200

    React 'React_Testing__01' !  install react-query  !

--- src/FetchCoursid/FetchCoursid.jsx
+++ src/FetchCoursid/FetchCoursid.jsx
@@ -1,11 +1,10 @@
 ﻿const fetchCoursid = async () => {
-  // const data = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11").then(
-  //   (response) => response.json()
-  // );
-  // console.log("fetchCoursid", data);
-  // // console.log(data);
-  // return data;
-  console.log("HI fetchCoursid");
+  const data = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11").then(
+    (response) => response.json()
+  );
+  console.log("fetchCoursid", data);
+  // console.log(data);
+  return data;
 };
 
 export default fetchCoursid;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { createProxy } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.privatbank.ua",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+})
