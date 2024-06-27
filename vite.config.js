8986a9d04260618ba65b6a8d4d5c1b0f49f80bc0
AndriiKot <a_kocyuba@ukr.net>
2024-06-27 15:07:42 +0200

    React 'React_Testing__01' !  testing  http-proxy-middleware  1 !

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createProxy } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.privatbank.ua",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
